0f327b0324dfda74853ddc1e9c43d302b4a6caa8
chikezie vincent <chikezievincent7@gmail.com>
2023-01-19 12:15:26 -0800

    updated_py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,64 +1,74 @@
 #!/usr/bin/python3
-"""Define Rectangle Class
-"""
-
+"""Module that defines a square object"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Module Representation of Square
-"""
+    """Defines a square class"""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization a Square
+        """Method that initialized the square
+        Args:
+           size: side's size of the square
+           x: Position on x axis.
+           y: Position on y axis.
+        Return:
+           Always nothing.
         """
         super().__init__(size, size, x, y, id)
 
+    def __str__(self):
+        """Method that returns a string"""
+        return ("[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                  self.width))
+
     @property
     def size(self):
-        """module Square size getter
+        """Getter the size of the square
         """
         return self.width
 
     @size.setter
     def size(self, value):
-        """module Square size setter
+        """Setter the size of the square
+        Args:
+           value: Size to assign
+        Return:
+           Always Nothing
         """
         self.width = value
-        self.height = value
-
-    def __str__(self):
-        """module string represation of square
-        """
-        return "[Square] ({:d}) {:d}/{:d} - {:d}".format(self.id,
-                                                         self.x,
-                                                         self.y,
-                                                         self.width)
+        self.heigth = value
 
     def update(self, *args, **kwargs):
-        """module update square
+        """Method that update arguments for square object
+        Args:
+           *args: list of arguments.
+           **kwargs: Dictionary of the arguments.
+        Return:
+           Always nothing
         """
-        if len(args):
-            for i, arg in enumerate(args):
-                if i == 0:
-                    self.id = arg
-                elif i == 1:
-                    self.size = arg
-                elif i == 2:
-                    self.x = arg
-                elif i == 3:
-                    self.y = arg
+        dict_order = ['id', 'size', 'x', 'y']
+        if args is not None and bool(args) is True:
+            i = 0
+            for key in dict_order:
+                try:
+                    setattr(self, key, args[i])
+                except IndexError:
+                    pass
+                i += 1
         else:
-            for key, value in kwargs.items():
-                if hasattr(self, key) is True:
-                    setattr(self, key, value)
+            for key in dict_order:
+                try:
+                    setattr(self, key, kwargs[key])
+                except KeyError:
+                    pass
 
     def to_dictionary(self):
-        """retrun dictonary
+        """Method that returns the dictionary
+           representation of a Square.
         """
-        return {
-            "id": self.id,
-            "size": self.size,
-            "x": self.x,
-            "y": self.y
-        }
+        dict_order = ['id', 'x', 'size', 'y']
+        dict_attrs = {}
+        for key in dict_order:
+            dict_attrs[key] = getattr(self, key)
+        return dict_attrs
